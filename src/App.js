3099b97a47d079aaae33981cbb721310c4f35582
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-02-18 00:20:23 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+var date = new Date()
+const [range, setRange] = useState(1)
+const [input, setInput] = useState(0)
+
+date.setDate(date.getDate() + input)
+
+function minusHandler(){
+  if(input === 0){
+    return
+  }
+  setInput(input-range)
+}
+
+
+function plusHandler(){
+setInput(range + input)
+}
+function resetHandler(){
+  setInput(0)
+  setRange(1)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-div'>
+      <input value={range} type='range' onChange={(e) => setRange(Number(e.target.value))} min={0} max={10} />
+      <span className='value'>{range}</span>
+    <div className='calculate'>
+    <button onClick={minusHandler} className='minus'>-</button>
+  <input value={input} onChange={(e) => setInput(Number(e.target.value))} className='input'   placeholder ="....." type='text'/>
+  <button onClick={plusHandler} className='plus'>+</button>
+    </div>
+    <p className='date'> {input ===  0  ? `today Is` :  `${input} Days from Today Is`} {date.toDateString()}</p>
+    <button onClick={resetHandler} className='reset'>Reset</button>
     </div>
   );
 }
